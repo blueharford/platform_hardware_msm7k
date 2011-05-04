4887d97608a69d8d29066a200e71f10c98cf6e42
Joshua Seidel <jsseidel1@gmail.com>
2011-05-04 16:27:49 -0400

    msm7k: Add support for QCOM librpc
    Enable with BOARD_USES_QCOM_LIBRPC
    
    Change-Id: I200a07a3b582f445322646c19bc6346c52a4f771 (cherry-picking from commit 22075f07bcdee9f2375a4ceedd69922855a51049)
    gralloc: Add support for QCOM PMEM flush
    
     * Google uses a different ioctl than QCOM for flushing PMEM cache.
       Add BOARD_USE_QCOM_PMEM option in case we need to be compatible with
       a kernel that does not support PMEM_CACHE_FLUSH.
    
    Change-Id: Id4884a970c84603281dec06956917090406c17b4 (cherry-picking from commit 0a2dcded9d5d30db022bdb1367deb97eaff8e080)

--- Android.mk
+++ Android.mk
@@ -14,19 +14,43 @@
 # limitations under the License.
 #
 
-common_msm_dirs := libcopybit liblights libopencorehw librpc libstagefrighthw
-msm7k_dirs := $(common_msm_dirs) boot libgralloc libaudio
+LIBAUDIO := libaudio
+ifeq ($(TARGET_PROVIDES_LIBAUDIO),true)
+    # A 'true' value assumes it is present in the device's dir,
+    # so exclude it here entirely to avoid duplicates
+    LIBAUDIO := ""
+else ifneq ($(TARGET_PROVIDES_LIBAUDIO),)
+    # Target provides a full path to its libaudio
+    LIBAUDIO := $(TARGET_PROVIDES_LIBAUDIO)
+endif
+
+LIBRPC := librpc
+ifeq ($(BOARD_USES_QCOM_LIBRPC),true)
+    LIBRPC := librpc-qcom
+endif
+
+common_msm_dirs := libcopybit liblights libopencorehw $(LIBRPC) libstagefrighthw
+msm7k_dirs := $(common_msm_dirs) boot libgralloc $(LIBAUDIO)
+msm7k_adreno_dirs := $(common_msm_dirs) boot libgralloc-qsd8k $(LIBAUDIO)
 qsd8k_dirs := $(common_msm_dirs) libgralloc-qsd8k libaudio-qsd8k dspcrashd
-msm7x30_dirs := liblights libgralloc-qsd8k librpc libaudio-qdsp5v2
+msm7x30_dirs := liblights libgralloc-qsd8k $(LIBRPC) libaudio-qdsp5v2 liboverlay
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
   include $(call all-named-subdir-makefiles,$(msm7k_dirs))
 else
-  ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+    ### QSD8k
     include $(call all-named-subdir-makefiles,$(qsd8k_dirs))
-  else
-    ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
-      include $(call all-named-subdir-makefiles,$(msm7x30_dirs))
-    endif
-  endif
+else ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),adq)
+    ### "adq" board (7x25)
+    include $(call all-named-subdir-makefiles,$(common_msm_dirs))
+else ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+    ### MSM7x30
+    include $(call all-named-subdir-makefiles,$(msm7x30_dirs))
+else ifeq ($(TARGET_BOARD_PLATFORM_GPU),qcom-adreno200)
+    ### MSM7k with Adreno GPU
+    include $(call all-named-subdir-makefiles,$(msm7k_adreno_dirs))
+else ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
+    ### Other MSM7k
+    include $(call all-named-subdir-makefiles,$(msm7k_dirs))
 endif
